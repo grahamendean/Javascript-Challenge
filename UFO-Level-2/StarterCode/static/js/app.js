f816574cf7413c03567447a1b54217083ff67fed
graham endean <grahamendean@grahams-mbp.attlocal.net>
2020-09-23 21:55:23 -0400

    Final HW Push

--- UFO-Level-2/StarterCode/static/js/app.js
+++ UFO-Level-2/StarterCode/static/js/app.js
@@ -21,11 +21,10 @@ showTable(data);
 var form = d3.select('form');
 // form.on("change",runEnter);    
 var button = d3.select('button');
-button.on('click', runEnter);
+button.on('click', handleClick);
 var inputs = d3.selectAll('input');
 //clicking function
 inputs.on('change', runEnter)
-
 
 var filteredData = data;
 
@@ -34,41 +33,21 @@ function runEnter() {
     var key = d3.select(this).property('id');
     var value = d3.select(this).property('value');
 
+    console.log(key,value);
+    
     if(value){
-        filteredData.filter(obj => obj[key] == value);
+        filteredData = filteredData.filter(obj => obj[key] == value);
     };
 
     showTable(filteredData);
 }
 
+function handleClick() {
+    d3.event.preventDefault();
+    inputs.property('value','');
+    filteredData = data;
+    showTable(data);
+}
 
 
-
-
-//     d3.event.preventDefault();
-//     tbody.html('');
-//     var date_time = d3.select("#datetime");
-//     var value = date_time.property('value');
-//     var city = d3.select("#city");
-//     var city_value = city.property('value');
-//     var state = d3.select("#state");
-//     var state_value = state.property('value');
-//     var country = d3.select("#country");
-//     var country_value = country.property('value');
-//     var shape = d3.select("#shape");
-//     var shape_value = shape.property('value');
-    
-//     var filterData = data.filter(obj => (obj.datetime === value) || (obj.city === city_value) || 
-//         (obj.state === state_value) || (obj.country === country_value) || (obj.shape === shape_value));
-//     console.log(filterData)
-// //filter the data 
-//     filterData.forEach(function(text_input) {
-//         var row = tbody.append("tr");
-//         Object.entries(text_input).forEach(function([key, value]) {
-//             console.log(key,value);
-//             var cell = row.append("td");
-//             cell.text(value);
-//         })
-//     })
-// };
 
